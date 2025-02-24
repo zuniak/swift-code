0b9ebaadb8d09cd69141de817c035d15f01e859e
Zuzia <zstajniak@student.agh.edu.pl>
2025-02-24 15:08:23 +0100

    Initial commit

---
+++ src/main/java/com/example/swift_code/BankSwiftApplication.java
@@ -0,0 +1,12 @@
+package com.example.swift_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSwiftApplication.class, args);
+	}
+}
